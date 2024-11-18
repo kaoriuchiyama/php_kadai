3a3e8bd231a2007e8f027bcacb355afc6526a8b1
kaori uchiyama <kaoriuchiyama2709@gmail.com>
2024-11-18 21:41:21 +0900

    課題修正

--- lesson4.php
+++ lesson4.php
@@ -35,26 +35,39 @@
  echo "\n";
 
  //問４
- $A = 1;
- $B = 2;
- $C = 3;
- $D = 4;
- $E = 5;
- echo max($A,$B,$C,$D,$E); //最大値の表示
+//  $A = 1;
+//  $B = 2;
+//  $C = 3;
+//  $D = 4;
+//  $E = 5;
+//  echo max($A,$B,$C,$D,$E); //最大値の表示
+//  echo "\n";
+
+//  echo "\n";
+
+ //for文を使った最大値の表示例
+ $number = array(3,4,6,7,8); //配列に数を用意
+ $max = $number[0]; //表示する最大値に配列の最初の数値を代入（初期化）
+ for ($i = 1;$i < count($number);$i++){ //二番目から配列の最後までをループ処理。count:要素の数
+   if ($max<$number[$i]){ //最大値に代入した数値より大きければ上書き
+       $max = $number[$i]; 
+   }
+ }
+ echo $max;
  echo "\n";
 
  echo "\n";
 
+ 
+//問５
 
- //問５
-
- $kaibun ='たけやぶやけた';
- $text_kaibun = mb_strlen($kaibun, 'UTF-8'); //文字列の長さを取得、マルチバイトを使用
+ $text ='たけやぶやけた';
+ $palindrome = mb_strlen($text, 'UTF-8'); //文字列の長さを取得、マルチバイトを使用
  // var_dump ($text_kaibun);
  $array = []; //配列にする
- for ($i = 0; $i < $text_kaibun; $i++){ 
-    $kotae = mb_substr($kaibun, $i, 1); //i番目の文字から1文字ずつ取り出して処理
-    $array[] = $kotae; //取得した文字を配列に追加
+ for ($i = 0; $i < $palindrome; $i++){ 
+    $answer = mb_substr($text, $i, 1); //i番目の文字から1文字ずつ取り出して処理
+    $array[] = $answer; //取得した文字を配列に追加
  }
  //echo implode(array_reverse($array));  //逆順で表示できているか（文字化けなど）
  if ((array_reverse($array)) === $array){  //逆順との比較、演算子”＝＝＝”完全一致してるか確認
